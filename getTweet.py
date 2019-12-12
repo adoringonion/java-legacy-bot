6eedbef2a3d840dd7c8fc0251b5cb40cd99fc38f
Fumihito Morita <adringonion.byazura@gmail.com>
2019-12-12 11:13:10 +0900

    some changes

--- getTweet.py
+++ getTweet.py
@@ -1,40 +1,69 @@
 import json
-import config
+import os
 from requests_oauthlib import OAuth1Session
 from datetime import datetime, timezone, timedelta
 from dateutil import parser
 from pytz import timezone
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
-CK = config.API_KEY
-CS = config.API_SECRET_KEY
-AT = config.ACCESS_TOKEN
-ATS = config.ACCESS_TOKEN_SECRET
-twitter = OAuth1Session(CK, CS, AT, ATS)
 
-url = 'https://api.twitter.com/1.1/search/tweets.json'
-keyword = 'Java　レガシー'
-count = 0
-
-params = {
-    'count' : 100,
-    'q' : keyword
-}
-
-req = twitter.get(url, params = params)
-
-if req.status_code == 200:
-    res = json.loads(req.text)
-    print(len(res['statuses']))
-
-    for tweet in res['statuses']:
-        print(datetime.now() - parser.parse(tweet['created_at']).replace(tzinfo=None))
-        print("-" * 10)
-
-        if datetime.now() - parser.parse(tweet['created_at']).replace(tzinfo=None) <= timedelta(hours=1):
-            count += 1
-        
+def lambda_handler(event, context):
     
-    print(count)
+    count = 0
+    count = search_tweet(count)
+    
+    put_item(count)
 
-else:
-    print('Faiiled: %d' % req.status_code)
+
+def search_tweet(count):
+    
+    CK  = os.environ['CONSUMER_KEY']
+    CS  = os.environ['CONSUMER_SECRET_KEY']
+    AT  = os.environ['ACCESS_TOKEN']
+    ATS = os.environ['ACCESS_TOKEN_SECRET']
+    twitter = OAuth1Session(CK, CS, AT, ATS)
+    
+    url = 'https://api.twitter.com/1.1/search/tweets.json'
+    query = 'Java　レガシー'
+    
+    params = {
+        'count' : 100,
+        'q' : query
+    }
+    
+    req = twitter.get(url, params = params)
+    
+    print('currentTime:', datetime.now())
+    
+    if req.status_code == 200:
+        res = json.loads(req.text)
+    
+        for tweet in res['statuses']:
+            print("-" * 20)
+            print('timeLag:',datetime.now() - parser.parse(tweet['created_at']).replace(tzinfo=None))
+            
+    
+            if datetime.now() - parser.parse(tweet['created_at']).replace(tzinfo=None) <= timedelta(hours=1):
+                count += 1
+    
+    else:
+        print('Faiiled: %d' % req.status_code)
+    
+    return count
+    
+    
+def put_item(count):
+    
+    dynamodb = boto3.resource('dynamodb')
+    
+    get_current_time = datetime.now() + timedelta(hours=9)
+        
+    table = dynamodb.Table('CountTweets')
+    table.put_item (
+        Item={
+            "Date": get_current_time.strftime('%Y-%m-%d'),
+            "Time": get_current_time.strftime('%H:%M:%S'),
+            "CountTweets": count
+        }
+    )
